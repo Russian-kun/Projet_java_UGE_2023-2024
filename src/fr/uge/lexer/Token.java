687a4e9d8353059b5f6f7fe4a636d23f30515c95
Russian-kun <94357324+Russian-kun@users.noreply.github.com>
2023-11-23 14:26:02 +0100

    Add lexer

---
+++ src/fr/uge/lexer/Token.java
@@ -0,0 +1,24 @@
+package fr.uge.lexer;
+
+public enum Token {
+  IDENTIFIER("[A-Za-z]+"),
+  NUMBER("[0-9]+"),
+  LEFT_PARENS("\\("),
+  RIGHT_PARENS("\\)"),
+  LEFT_BRACKET("\\["),
+  RIGHT_BRACKET("\\]"),
+  COMMA(","),
+  COLON(":"),
+  QUOTE("\"\"\"[^\"]+\"\"\""),
+  ;
+
+  private final String regex;
+
+  Token(String regex) {
+    this.regex = regex;
+  }
+
+  String regex() {
+    return regex;
+  }
+}
