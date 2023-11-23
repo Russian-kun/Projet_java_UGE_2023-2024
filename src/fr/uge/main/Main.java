62766befdc65e5901f8c00717925521d42722708
Russian-kun <94357324+Russian-kun@users.noreply.github.com>
2023-11-23 14:48:14 +0100

    Update Lexer import and file path

--- src/fr/uge/main/Main.java
+++ src/fr/uge/main/Main.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import fr.uge.lexer.*;
+import fr.uge.lexer.Lexer;
+import fr.uge.lexer.Result;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    var path = Path.of("big.map");
+    var path = Path.of("maps/demo1.map");
     var text = Files.readString(path);
     var lexer = new Lexer(text);
     Result result;
