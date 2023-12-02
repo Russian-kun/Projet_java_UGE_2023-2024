abb44112472a16dc931fc2d247d44c53da5ffebb
Russian-kun <compte.perso.russian@gmail.com>
2023-12-02 19:42:31 +0100

    Add import

--- src/fr/uge/TheBigAventure/World.java
+++ src/fr/uge/TheBigAventure/World.java
@@ -5,6 +5,7 @@ import fr.uge.lexer.Result;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
