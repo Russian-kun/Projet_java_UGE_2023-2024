a21e61c190befbeaf6d732dcec3f1bc2a5b12e76
Russian-kun <compte.perso.russian@gmail.com>
2023-12-06 14:35:34 +0100

    Better lastLexer

--- src/fr/uge/lexer/Lexer.java
+++ src/fr/uge/lexer/Lexer.java
@@ -1,14 +1,15 @@
 package fr.uge.lexer;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.util.stream.Collectors.joining;
-
 public class Lexer {
   private static final List<Token> TOKENS = List.of(Token.values());
+  // Pour chaque pattern, on fait : (regex) | (regex) | ...
   private static final Pattern PATTERN = Pattern.compile(
       TOKENS.stream()
           .map(token -> "(" + token.regex() + ")")
@@ -16,6 +17,8 @@ public class Lexer {
 
   private final String text;
   private final Matcher matcher;
+
+  private Result lastResult = null;
 
   public Lexer(String text) {
     this.text = Objects.requireNonNull(text);
@@ -32,22 +35,32 @@ public class Lexer {
       if (start != -1) {
         var end = matcher.end(group);
         var content = text.substring(start, end);
-        return new Result(TOKENS.get(group - 1), content);
+        lastResult = new Result(TOKENS.get(group - 1), content, start, end);
+        return lastResult;
       }
     }
     throw new AssertionError();
   }
 
   public Result lastResult() {
-    for (var group = 1; group <= matcher.groupCount(); group++) {
-      var start = matcher.start(group);
-      if (start != -1) {
-        var end = matcher.end(group);
-        var content = text.substring(start, end);
-        return new Result(TOKENS.get(group - 1), content);
-      }
-    }
-    throw new AssertionError();
+    return lastResult;
+    // for (var group = 1; group <= matcher.groupCount(); group++) {
+    // var start = matcher.start(group);
+    // if (start != -1) {
+    // var end = matcher.end(group);
+    // var content = text.substring(start, end);
+    // return new Result(TOKENS.get(group - 1), content, start, end);
+    // }
+    // }
+    // throw new AssertionError();
+  }
+
+  public String text() {
+    return text;
+  }
+
+  public Matcher matcher() {
+    return matcher;
   }
 
 }
