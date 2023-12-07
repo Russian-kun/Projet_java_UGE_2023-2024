354ae9aa438f9ac1b1a2952df1675a8062220da4
Russian-kun <compte.perso.russian@gmail.com>
2023-12-07 18:18:39 +0100

    Add new obstacle types and check passability

--- src/fr/uge/TheBigAventure/objects/Obstacles.java
+++ src/fr/uge/TheBigAventure/objects/Obstacles.java
@@ -7,13 +7,32 @@ import fr.uge.TheBigAventure.general.Position;
 public class Obstacles extends Element {
   private String name;
 
-  public enum ObstacleType {
+  public enum ImpassableType implements ObstacleType {
     BED, BOG, BOMB, BRICK, CHAIR, CLIFF, DOOR, FENCE, FORT, GATE, HEDGE, HOUSE, HUSK, HUSKS, LOCK, MONITOR, PIANO,
-    PILLAR, PIPE, ROCK, RUBBLE, SHELL, SIGN, SPIKE, STATUE, STUMP, TABLE, TOWER, TREE, TREES, WALL
+    PILLAR, PIPE, ROCK, RUBBLE, SHELL, SIGN, SPIKE, STATUE, STUMP, TABLE, TOWER, TREE, TREES, WALL,
+    ICE, LAVA, WATER
+  }
+
+  public enum PassableType implements ObstacleType {
+    ALGAE, CLOUD, FLOWER, FOLIAGE, GRASS, LADDER, LILY, PLANK, REED, ROAD, SPROUT, TILE, TRACK, VINE
   }
 
   public Obstacles(String name, ObstacleType skin, Position position) {
     super(skin.toString(), position, Element.Kind.OBSTACLE);
+    this.name = name;
+  }
+
+  public Obstacles(String name, String skin, Position position) {
+    super(skin.toString(), position, Element.Kind.OBSTACLE);
+    try {
+      ImpassableType.valueOf(skin.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      try {
+        PassableType.valueOf(skin.toUpperCase());
+      } catch (IllegalArgumentException e2) {
+        throw new IllegalArgumentException("skin must be an obstacle : " + skin + " is not an obstacle type");
+      }
+    }
     this.name = name;
   }
 
@@ -23,5 +42,19 @@ public class Obstacles extends Element {
 
   public String getName() {
     return name;
+  }
+
+  public String skin() {
+    return skin;
+  }
+
+  public static boolean isPassable(Obstacles obstacle) {
+    if (obstacle == null)
+      return true;
+    try {
+      return PassableType.valueOf(obstacle.skin().toUpperCase()) != null;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
