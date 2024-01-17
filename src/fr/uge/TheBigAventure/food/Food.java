726e97acf4916a15ad1a65cef5d1301bf6118bfe
Russian-kun <94357324+Russian-kun@users.noreply.github.com>
2024-01-17 18:24:54 +0100

    Add import statement and convert method to Food class

--- src/fr/uge/TheBigAventure/food/Food.java
+++ src/fr/uge/TheBigAventure/food/Food.java
@@ -1,5 +1,7 @@
 package fr.uge.TheBigAventure.food;
 
+import java.util.Arrays;
+
 import fr.uge.TheBigAventure.gameObjects.InventoryObjectSkin;
 import fr.uge.TheBigAventure.general.Position;
 
@@ -21,6 +23,10 @@ public class Food extends GeneralFood {
 
     public GeneralFoodType getFoodSkin() {
       return type;
+    }
+
+    public static FoodType convert(GeneralFoodType old) {
+      return Arrays.stream(FoodType.values()).filter(type -> type.getFoodSkin() == old).findFirst().orElse(null);
     }
   }
 
